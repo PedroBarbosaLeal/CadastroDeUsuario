1c1008aa9f844b1caacd938cf5725b826d2c3576
Pedro Leal <80418208+PedroBarbosaLeal@users.noreply.github.com>
2025-07-22 17:01:25 -0300

    atualizacoes

--- src/main/java/org/example/cadastroDeUsuario/repository/UsuarioRepository.java
+++ src/main/java/org/example/cadastroDeUsuario/repository/UsuarioRepository.java
@@ -1,6 +1,4 @@
 package org.example.cadastroDeUsuario.repository;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import org.example.cadastroDeUsuario.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -8,6 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UsuarioRepository extends JpaRepository<Usuario,Long> {
 
     boolean existsByEmail(String email);
-
-    boolean existsByNome(String nome);
 }
